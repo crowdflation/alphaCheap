54b323fd4973735f8a3c167a5a245f12a36790cb
Ignas <ignas@spottycash.net>
2021-08-28 04:00:49 +0100

    add Kroger to vendors

--- background.js
+++ background.js
@@ -1,9 +1,18 @@
 // Regex-pattern to check URLs against.
 // TODO: make this an array
-var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?walmart\.com\/grocery\/*/;
+const vendors = {
+  walmart: {
+    urlRegex: /^https?:\/\/(?:[^./?#]+\.)?walmart\.com\/grocery\/*/,
+    backend: 'https://mflation.herokuapp.com/api/walmart'
+  },
+  kroger: {
+    urlRegex: /^https?:\/\/(?:[^./?#]+\.)?kroger\.com\/search*/,
+    backend: 'https://mflation.herokuapp.com/api/kroger'
+  },
+};
 
-// A function to use as callback
-function doStuffWithDom(domContent) {
+
+function handleResponse(response) {
   let location = null;
   if ('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(function(position) {
@@ -11,22 +20,27 @@ function doStuffWithDom(domContent) {
     });
   }
 
-  let data = JSON.parse(domContent);
+  let { data, vendor } = JSON.parse(response);
 
+  // Send data to website
+  // TODO: This will send data to blockchain directly later on
   var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
-  xmlhttp.open("POST", 'https://mflation.herokuapp.com/api/walmart');
+  xmlhttp.open("POST", vendors[vendor].backend);
   xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   xmlhttp.send(JSON.stringify({data, location}));
 }
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if(changeInfo?.status =='complete') {
-    if (urlRegex.test(tab.url)) {
-      // haven't figured out a better way to do this yet, so we just wait 5 seconds after page is loaded
-      setTimeout(()=> {
-        // ...if it matches, send a message specifying a callback too
-        chrome.tabs.sendMessage(tab.id, {text: 'walmart'}, doStuffWithDom)
-      }, 5000);
+    for(let key in vendors) {
+      const vendor = vendors[key];
+      if (vendor.urlRegex.test(tab.url)) {
+        // haven't figured out a better way to do this yet, so we just wait 5 seconds after page is loaded
+        setTimeout(()=> {
+          // ...if it matches, send a message specifying a callback too
+          chrome.tabs.sendMessage(tab.id, {text: key}, handleResponse)
+        }, 5000);
+      }
     }
   }
 });
--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 // TODO: Move this into separate file
-function parseWalmart(document, sendResponse) {
+function parseWalmart(document, sendResponse, vendor) {
   const products = document.querySelectorAll('[data-automation-id="productTileDetails"]');
   let result = [];
   [].forEach.call(products, function (product) {
@@ -11,19 +11,43 @@ function parseWalmart(document, sendResponse) {
       return;
     }
     const parsedData = {pricePerUnit, price, name};
-    console.log(parsedData);
     result.push(parsedData);
   });
   // Pass it back
-  sendResponse(JSON.stringify(result, null, 2));
+  sendResponse(JSON.stringify({ data: result, vendor}, null, 2));
+}
+
+function parseKroger(document, sendResponse, vendor) {
+  const products = document.querySelectorAll('[data-qa*="product-card-"]');
+  let result = [];
+  [].forEach.call(products, function (product) {
+    const price = product.querySelectorAll('[data-qa="cart-page-item-unit-price"]')[0]?.innerText;
+    const nameAndPrice = product.querySelectorAll('[data-qa="cart-page-item-description"]')[0].innerText;
+    const name = nameAndPrice.split(' - ')[0];
+    const pricePerUnit = nameAndPrice.split(' - ')[1];
+
+    //Ignore if we don't have price data
+    if (!pricePerUnit && !price) {
+      return;
+    }
+    const parsedData = {pricePerUnit, price, name};
+    result.push(parsedData);
+  });
+  // Pass it back
+  sendResponse(JSON.stringify({ data: result, vendor}, null, 2));
 }
 
 // Listen for messages from background script
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   // If the received message has the expected format...
   // TODO: this should run different script depending on which page is being parsed
+  // Add more vendors
   if (msg.text === 'walmart') {
     // Parse the data
-    parseWalmart(document, sendResponse);
+    parseWalmart(document, sendResponse, 'walmart');
+  } else if (msg.text === 'kroger') {
+    // Parse the data
+    parseKroger(document, sendResponse, 'kroger');
   }
+
 });
