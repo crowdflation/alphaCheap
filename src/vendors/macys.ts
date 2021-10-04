954c8c6c103a96bb8c7ff7ecbea044a93e04e268
mat <mat.bck@protonmail.com>
2021-10-04 23:22:00 +0200

    add macys and doordash vendors

---
+++ src/vendors/macys.ts
@@ -0,0 +1,31 @@
+const vendor = 'macys';
+function parse(document, sendResponse) {
+  const products = document.querySelectorAll('div.productDetail');
+  let result:any[] = [];
+  [].forEach.call(products, function (product: any) {
+    try {
+      const originalPrice = product.querySelectorAll('.priceInfo .regular > span')[0]?.innerText;
+      let price = product.querySelectorAll('.priceInfo .discount > span')[0]?.innerText;
+      if (!price){
+        price = product.querySelectorAll('.priceInfo .regular > span')[1]?.innerText;
+      }
+      const name = product.querySelectorAll('div.productDescription > a')[0].innerText;
+      const brand = product.querySelectorAll('div.productBrand')[0].innerText;
+      //Ignore if we don't have price data
+      if (!originalPrice&&!price) {
+        return;
+      }
+      const parsedData = {originalPrice, price, brand, name};
+      result.push(parsedData)
+    }
+    catch (e) {
+      console.error('Did not parse one items', e);
+    }
+  });
+  // Pass it back
+  sendResponse(JSON.stringify({ data: result, vendor}, null, 2));
+}
+
+const urlRegex = /^https?:\/\/(?:[^./?#]+\.)?macys\.com\/shop\/*/;
+const backend = 'https://mflation.herokuapp.com/api/macys';
+export default {vendor, parse, urlRegex, backend};
