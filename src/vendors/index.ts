a994b8c01f76113f451bcd66b4ce6f9a8ef47de6
Ignas Galvelis <igalvelis+inf@gmail.com>
2022-01-23 06:00:55 +0000

    Remove vendor that is saved on the server

--- src/vendors/index.ts
+++ src/vendors/index.ts
@@ -9,7 +9,6 @@ import walmart from './walmart';
 import zillow from './zillow';
 import macys from './macys';
 import doordash from './doordash';
-import albertsons from "./albertsons";
 import apartments from "./apartments";
 
-export default {kroger, walmart, zillow, macys, doordash, albertsons, apartments} as {[name:string]:IVendor};
+export default {kroger, walmart, zillow, macys, doordash, apartments} as {[name:string]:IVendor};
